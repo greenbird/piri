730a58de1a810f62cff49ba1e9da1b0600322a38
Thomas Borgen <thomas@borgenit.no>
2020-08-04 14:13:52 +0200

    adds a jsonschema validator function.

---
+++ mapmallow/schema.py
@@ -0,0 +1,26 @@
+from typing import List, final
+
+from attr import dataclass
+from jsonschema import ValidationError
+from jsonschema.schemas import Validator
+from returns.result import Failure, Result, Success
+
+
+@final
+@dataclass(frozen=True, slots=True)
+class SchemaValidator(object):
+    """Validates data with given JsonSchema Validator."""
+
+    validator: Validator
+
+    def __call__(
+        self,
+        schema_data: dict,
+    ) -> Result[dict, List[ValidationError]]:
+        """If is valid return success(schema_data) otherwise list of errors."""
+        if self.validator.is_valid(schema_data):
+            return Success(schema_data)
+
+        return Failure(
+            list(self.validator.iter_errors(schema_data)),
+        )
