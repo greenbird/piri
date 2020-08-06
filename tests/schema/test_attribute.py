4f0db3269e6f573282f3e7e01252f878e83681f5
Thomas Borgen <thomas@borgenit.no>
2020-08-06 15:02:47 +0200

    adds jsonschema Validator function and tests

--- tests/schema/test_attribute.py
+++ tests/schema/test_attribute.py
@@ -2,17 +2,17 @@ from jsonschema import ValidationError
 from returns.pipeline import is_successful
 
 
-def test_validates(casting_validator, valid):
+def test_validates(attribute_validator, valid):
     """Test that we get dict back on valid validation."""
-    test: dict = {'to': 'integer'}
-    assert casting_validator(test).unwrap() == test
+    test: dict = {'name': 'attrib', 'default': 'bob'}
+    assert attribute_validator(test).unwrap() == test
 
 
-def test_invalid(casting_validator, invalid):
+def test_invalid(attribute_validator, invalid):
     """Test that we get a list of errors."""
-    test: dict = {'to': 'date'}
-    validate_result = casting_validator(test)
+    test: dict = {'seperator': ' '}
+    validate_result = attribute_validator(test)
     assert not is_successful(validate_result)
     assert isinstance(validate_result.failure(), list)
     assert isinstance(validate_result.failure()[0], ValidationError)
-    assert 'original_format' in str(validate_result.failure())
+    assert 'name' in str(validate_result.failure())
--- tests/schema/test_object.py
+++ tests/schema/test_object.py
@@ -1,31 +1,16 @@
-import json
-
-from jsonschema import Draft7Validator, ValidationError
+from jsonschema import ValidationError
 from returns.pipeline import is_successful
 
-from mapmallow.schema import SchemaValidator
 
-schema = {}
-with open(
-    'mapmallow/schema.json',
-    'r',
-) as schema_file:
-    schema = json.load(schema_file)
-
-validator = SchemaValidator(
-    Draft7Validator(schema=schema),
-)
-
-
-def test_validates(valid):
+def test_validates(object_validator, valid):
     """Test that we get dict back on valid validation."""
-    result = validator(valid)
-    assert result.unwrap() == valid
+    validate_result = object_validator(valid)
+    assert validate_result.unwrap() == valid
 
 
-def test_invalid(invalid):
+def test_invalid(object_validator, invalid):
     """Test that we get a list of errors."""
-    result = validator(invalid)
-    assert not is_successful(result)
-    assert isinstance(result.failure(), list)
-    assert isinstance(result.failure()[0], ValidationError)
+    validate_result = object_validator(invalid)
+    assert not is_successful(validate_result)
+    assert isinstance(validate_result.failure(), list)
+    assert isinstance(validate_result.failure()[0], ValidationError)
