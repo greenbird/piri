0378d840aa13fee1e40c45c228b17a757c2e057f
Thomas Borgen <thomas@borgenit.no>
2020-08-07 14:17:31 +0200

    moves apply_casting tests

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -16,36 +16,6 @@ from mapmallow.functions import (
 )
 
 
-
-class TestCasting(object):
-    """Test casting function general error handling."""
-
-    _cast = ApplyCasting()
-
-    def test_no_value_raises_fails(self):
-        """No cast_to string should just return input value."""
-        assert self._cast('val', {'to': None}).unwrap() == 'val'
-
-    def test_value_but_cast_to_fails(self):
-        """No value should just fail with ValueError."""
-        test = self._cast(None, {})
-        assert not is_successful(test)
-        assert isinstance(test.failure(), ValueError)
-        assert 'value_to_cast is empty' in str(test.failure())
-
-    def test_not_implemented_cast_to(self):
-        """Test that we get NotImplementedError for unknown cast_to's."""
-        test = self._cast('val', {'to': 'not_supported'})
-        assert not is_successful(test)
-        assert isinstance(test.failure(), NotImplementedError)
-        assert 'Unsupported cast to value' in str(test.failure())
-
-    def test_no_cast_to_returns_value(self):
-        """Test that when we do not provide cast_to we get original value."""
-        test = self._cast('val', {'to': None})
-        assert test.unwrap() == 'val'
-
-
 class TestCastingInteger(object):
     """Do extensive testing of our int casting functions."""
 
