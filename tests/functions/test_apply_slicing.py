b1b97f9ee844379940e60768b9e9fed7dd5cd95d
Thomas Borgen <thomas@borgenit.no>
2020-09-28 09:48:30 +0200

    adds tests for slicing with negative from and to values

--- tests/functions/test_apply_slicing.py
+++ tests/functions/test_apply_slicing.py
@@ -24,3 +24,13 @@ def test_slice_start_to_middle():
 def test_slice_start_to_end():
     """Test that we can slice from start to end."""
     assert apply_slicing('test', {'from': 0, 'to': None}) == 'test'
+
+
+def test_slice_negative_from():
+    """Test that a negative from value starts cutting at the end minus from."""
+    assert apply_slicing('012345', {'from': -2}) == '45'
+
+
+def test_slice_negative_to():
+    """Test that a negative to value ends cut at end minus to."""
+    assert apply_slicing('01234', {'from': 0, 'to': -2}) == '012'
