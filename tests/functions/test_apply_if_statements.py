2e19d9e2af14d7d99b2ba8e8d1517f21cdc3abd8
Thomas Borgen <thomas@borgenit.no>
2020-10-06 11:53:29 +0200

    adds if statement tests for array and object values

--- tests/functions/test_apply_if_statements.py
+++ tests/functions/test_apply_if_statements.py
@@ -33,6 +33,36 @@ def test_if_is_condition_false():
     assert apply_if_statements(*test) == Success('not_target_value')
 
 
+def test_if_is_condition_array_value():
+    """Test that we can do if is statement on arrays."""
+    test = [
+        ['target_value'],
+        [
+            {
+                'condition': 'is',
+                'target': ['target_value'],
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
+def test_if_is_condition_objects_value():
+    """Test that we can do if is statement on objectss."""
+    test = [
+        {'val': 'target'},
+        [
+            {
+                'condition': 'is',
+                'target': {'val': 'target'},
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
 def test_if_in():
     """Test that 1 if (in) statement works."""
     test = [
@@ -61,6 +91,36 @@ def test_if_in_condition_false():
         ],
     ]
     assert apply_if_statements(*test) == Success('not_target_value')
+
+
+def test_if_in_condition_array_value():
+    """Test that we can do if in statement on arrays."""
+    test = [
+        ['target_value'],
+        [
+            {
+                'condition': 'in',
+                'target': [['target_value']],
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
+def test_if_in_condition_objects_value():
+    """Test that we can do if is statement on objectss."""
+    test = [
+        {'val': 'target'},
+        [
+            {
+                'condition': 'in',
+                'target': [{'val': 'target'}],
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
 
 
 def test_if_not():
@@ -93,6 +153,36 @@ def test_if_not_condition_false():
     assert apply_if_statements(*test) == Success('target_value')
 
 
+def test_if_not_condition_array_value():
+    """Test that we can do if not statement on arrays."""
+    test = [
+        ['target_value'],
+        [
+            {
+                'condition': 'not',
+                'target': ['not_target'],
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
+def test_if_not_condition_objects_value():
+    """Test that we can do if not statement on objectss."""
+    test = [
+        {'val': 'target'},
+        [
+            {
+                'condition': 'not',
+                'target': {'val': 'tarnot'},
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
 def test_if_contains():
     """Test that 1 if (contains) statement works."""
     test = [
@@ -121,6 +211,36 @@ def test_if_contains_condition_false():
         ],
     ]
     assert apply_if_statements(*test) == Success('not_target_value')
+
+
+def test_if_contains_condition_array_value():
+    """Test that we can do if contains statement on arrays."""
+    test = [
+        ['value', 'target'],
+        [
+            {
+                'condition': 'contains',
+                'target': 'target',
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
+
+
+def test_if_contains_condition_objects_value():
+    """Test that we can do if contains statement on objectss."""
+    test = [
+        {'val': 'target'},
+        [
+            {
+                'condition': 'contains',
+                'target': 'val',
+                'then': 'value2',
+            },
+        ],
+    ]
+    assert apply_if_statements(*test) == Success('value2')
 
 
 def test_if_chained():
