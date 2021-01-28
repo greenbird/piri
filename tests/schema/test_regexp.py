8771051fb1f3e513cf37a9858a6f96f6b28ad7c5
Thomas Borgen <thomas@borgenit.no>
2021-01-28 19:55:49 +0100

    adds format checking on regex strings

--- tests/schema/test_regexp.py
+++ tests/schema/test_regexp.py
@@ -26,3 +26,13 @@ def test_wrong_search_keyword_type(regexp_validator):
     assert isinstance(validate_result.failure(), ValueError)
     assert isinstance(validate_result.failure().args[0][0], ValidationError)
     assert "123244 is not of type 'string'" in str(validate_result.failure())
+
+
+def test_invalid_regex_string(regexp_validator):
+    """Test that a normal string, but invalid regex expression fails."""
+    test: dict = {'search': '[a-zA-Z0-9%&'}
+    validate_result = regexp_validator(test)
+    assert not is_successful(validate_result)
+    assert isinstance(validate_result.failure(), ValueError)
+    assert isinstance(validate_result.failure().args[0][0], ValidationError)
+    assert "is not a 'regex'" in str(validate_result.failure())
